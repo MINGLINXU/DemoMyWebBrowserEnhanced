3057f175df5e617ce564497062990c52c02484cd
XUMINGLIN <18046506@myrp.edu.sg>
2019-08-02 13:40:48 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demomywebbrowser/MainActivity.java
@@ -0,0 +1,37 @@
+package sg.edu.rp.c346.demomywebbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadURL;
+    WebView wvMyPage;
+    EditText etURL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLoadURL=findViewById(R.id.buttonLoad);
+        etURL=findViewById(R.id.URL);
+
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = etURL.getText().toString();
+                etURL.setVisibility(View.GONE);
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+
+}
